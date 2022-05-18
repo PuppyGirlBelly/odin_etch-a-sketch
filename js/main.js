ef5e763b26b9c3b800d96c1b299504c03c3e7cda
SoftAnnaLee <jazzchamp600@gmail.com>
2022-05-18 16:30:36 -0600

    refactor code into smaller and simpler functions

--- js/main.js
+++ js/main.js
@@ -1,26 +1,54 @@
-const container = document.createElement('div');
-container.className = 'container';
-container.style.height = '100%';
-container.style.display = 'grid';
-container.style.gridTemplateRows = 'repeat(16, 1fr)';
-container.style.gridTemplateColumns = 'repeat(16, 1fr)';
-document.querySelector('body').append(container);
+function resetContainer(size) {
+  const container = document.querySelector('.container');
+  container.textContent = '';
+  container.style.display = 'grid';
+  container.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+  container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
 
-for (y = 0; y < 16; y++) {
-  for (x = 0; x < 16; x++) {
-    let square = document.createElement('div');
-    square.id = `${x}-${y}`;
-    square.className = 'square';
-    square.style.display = 'inline-block';
+  return container;
+}
 
-    square.addEventListener('mouseenter', () => {
-      square.style.backgroundColor = '#cecece';
-    });
+function createSquare() {
+  const square = document.createElement('div');
+  square.className = 'square';
 
-    square.addEventListener('mouseleave', () => {
-      square.style.backgroundColor = 'unset';
-    });
+  square.addEventListener('mouseenter', () => {
+    square.style.backgroundColor = '#cecece';
+  });
 
+  square.addEventListener('mouseout', () => {
+    square.style.backgroundColor = 'unset';
+  });
+
+  return square;
+}
+
+function drawGrid(size) {
+  const container = resetContainer(size);
+
+  for (let i = 0; i < size * size; i += 1) {
+    const square = createSquare();
     container.append(square);
   }
 }
+
+function populatePage() {
+  const container = document.createElement('div');
+  container.className = 'container';
+  container.style.height = '100vh';
+
+  const sizeButton = document.createElement('button');
+  sizeButton.id = 'size_button';
+  sizeButton.innerText = 'Click to resize/reset';
+  sizeButton.addEventListener('click', () => {
+    // eslint-disable-next-line no-alert
+    const newSize = prompt('Enter a size for the grid: ', 16);
+    drawGrid(newSize);
+  });
+
+  document.querySelector('body').append(sizeButton);
+  document.querySelector('body').append(container);
+}
+
+populatePage();
+drawGrid(16);
