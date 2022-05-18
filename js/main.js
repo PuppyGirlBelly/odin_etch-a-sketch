566f0367e53726317d7145a4a18ec8f3b91f6b22
SoftAnnaLee <jazzchamp600@gmail.com>
2022-05-18 16:26:27 -0600

    organize divs into grid

--- js/main.js
+++ js/main.js
@@ -1,18 +1,18 @@
 const container = document.createElement('div');
 container.className = 'container';
+container.style.height = '100vh';
+container.style.display = 'grid';
+container.style.gridTemplateRows = 'repeat(16, 1fr)';
+container.style.gridTemplateColumns = 'repeat(16, 1fr)';
+document.querySelector('body').append(container);
 
 for (y = 0; y < 16; y++) {
-  let row = document.createElement('div');
-  row.id = `row-${y}`;
-  row.className = 'row';
-  row.style.border = '1px solid red';
-  container.append(row);
   for (x = 0; x < 16; x++) {
     let square = document.createElement('div');
-    square.id = `square-${x}-${y}`;
+    square.id = `${x}-${y}`;
     square.className = 'square';
     square.style.border = '1px solid red';
+    square.style.display = 'inline-block';
     container.append(square);
   }
 }
-document.querySelector('body').append(container);
